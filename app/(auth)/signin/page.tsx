eeb054b678c8754122014c1804df84809c02ef1a
ritik7739 <ritik.kumar7739@gmail.com>
2024-09-10 08:37:45 +0530

    Add signup page client side file

---
+++ app/(auth)/signin/page.tsx
@@ -0,0 +1,10 @@
+import { SigninComponent } from "@/components/Signin";
+
+export default function Signin() {
+    return(
+        <div>
+            <SigninComponent/>
+        </div>
+    )
+}
+
--- app/layout.tsx
+++ app/layout.tsx
@@ -28,6 +28,9 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        <div className="p-4 border-b">
+          Medium
+        </div>
         {children}
       </body>
     </html>
